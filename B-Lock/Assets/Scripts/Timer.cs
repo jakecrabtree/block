44b2719b00f679b2de04c272ad6122d69bd25a8c
comannnnndooooo <kylecolburn38@gmail.com>
2018-09-15 16:13:48 -0500

    Timer is finished

--- B-Lock/Assets/Scripts/Timer.cs
+++ B-Lock/Assets/Scripts/Timer.cs
@@ -1,20 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
 
+    float timeLeft = 60;
+    Text timeRemainingText;
 	// Use this for initialization
 	void Start () {
-		
-	}
+		timeRemainingText = GetComponent<Text>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timeLeft -= Time.deltaTime;
+        timeRemainingText.text = Math.Round(timeLeft, 2).ToString();
 	}
 
 	void DecreaseTime(int amount){
-		
+        timeLeft -= amount;
 	}
 }
