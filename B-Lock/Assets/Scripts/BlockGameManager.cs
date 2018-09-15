5457b702cdb80810f506e73f4a729733c84d3ebd
Jake <jakercrabtree@utexas.edu>
2018-09-15 16:56:13 -0500

    added loading of objects into managers and ad skeleton

--- B-Lock/Assets/Scripts/BlockGameManager.cs
+++ B-Lock/Assets/Scripts/BlockGameManager.cs
@@ -7,9 +7,16 @@ public class BlockGameManager : MonoBehaviour {
 	Timer timer;
 	PageManager pageManager;
 
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+	}
+
 	// Use this for initialization
 	void Start () {
-		
+		pageManager.LoadPage();
 	}
 	
 	// Update is called once per frame
--- B-Lock/Assets/Scripts/PageManager.cs
+++ B-Lock/Assets/Scripts/PageManager.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 public class PageManager : MonoBehaviour {
 
+	AdManager adManager;
+	string resourcesPath = "Sprites" + Path.DirectorySeparatorChar + "Pages" + Path.DirectorySeparatorChar;
+	string[] pageBackgroundNames = {"rectangle_background"};
+	List<Sprite> pageBackgrounds;
+	int numPageBackgrounds;
+
+	void Awake(){
+		Instantiate(adManager);
+	}
+
 	// Use this for initialization
 	void Start () {
-		
+		foreach (string name in pageBackgroundNames){
+			string adImagePath = new StringBuilder(resourcesPath).Append(name).ToString();
+			pageBackgrounds.Add(Resources.Load<Sprite>(adImagePath));
+		}
+		numPageBackgrounds = pageBackgrounds.Count;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
+
+	public void LoadPage(){
 		
 	}
 }
