acff7079949b10851913acc346cd291dc7f56eb4
Jake <jakercrabtree@utexas.edu>
2018-09-16 12:06:01 -0500

    added alpha game

--- B-Lock/Assets/Scripts/Letter.cs
+++ B-Lock/Assets/Scripts/Letter.cs
@@ -20,4 +20,22 @@ public class Letter : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	/// <summary>
+	/// OnMouseDown is called when the user has pressed the mouse button while
+	/// over the GUIElement or Collider.
+	/// </summary>
+	void OnMouseDown()
+	
+	{
+		alphaClickAd.LetterClicked(letter);
+		Destroy(gameObject);
+	}
+
+	/// <summary>
+	/// This function is called when the MonoBehaviour will be destroyed.
+	/// </summary>
+	void OnDestroy()
+	{
+	}
 }
